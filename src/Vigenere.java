2f0669fd47eb7e8ebf66d29376716d1ce0589bdc
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-11-26 18:28:39 +0100

    Vigenere

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -1,35 +1,77 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Vigenere {
+    final String alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
-        String s = "AAAAAAA";
+        String s = "YYYYY";
         String pass = "AAA";
+        int a = 65;
 
-        pruebas(s, pass);
+        char c = 'Z';
+
+        int x = (c - a) + 1;
+        char z = (char) (a + x);
+
+        if(z > 90){
+            System.out.println((char) (z - 26));
+        }
+
+        System.out.println(z);
+
+        //System.out.println(pruebas(s, pass));
 
     }
 
-    static void pruebas (String s, String pass){
+    static String pruebas (String s, String pass){
         s = s.toUpperCase();
         pass = pass.toUpperCase();
+        int a = 65;
         StringBuilder res = new StringBuilder();
 
         if(s.length() > pass.length()){
-            String newPass = appending(s, pass);
+            String newPass = match(s, pass);
+
             for (int i = 0; i < s.length(); i++) {
-                res.append((char) (s.charAt(i) + pass.charAt(i)));
+
+                char c = s.charAt(i);
+
+                if (c >= 65 && c <= 90) {
+                    int letra = (c - a) + 1;
+
+                    if(c > 90){
+                         letra = (c - 26 - a) + 1;
+                        res.append((char) (letra + 1));
+                    }
+
+                    res.append((char) (letra + 1));
+
+                }
+
+            }
+
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                res.append((char) (pass.charAt(i) + 1));
             }
         }
 
         System.out.println(res.toString());
+
+        return res.toString();
+
     }
 
-    static String appending(String s, String pass){
+    // Igualamos la longitud de la password añadiendo la misma palabra varias veces
+    static String match(String s, String ss){
         StringBuilder res = new StringBuilder();
-
-        for (int i = 0; i < s.length(); i++) {
-            res.append(pass);
+        int cont = ss.length();
+        while(s.length() > cont){
+            res.append(ss);
+            cont++;
         }
+
         return res.toString();
     }
 
@@ -38,13 +80,16 @@ public class Vigenere {
         password = password.toUpperCase();
         StringBuilder res = new StringBuilder();
 
-        for (int i = 0; i < password.length(); i++) {
-            res.append((char) (s.charAt(i) + 1));
+        if(s.length() > password.length()){
+            String newPass = match(s, password);
+            for (int i = 0; i < s.length(); i++) {
+                res.append((char) (s.charAt(i) +1 ));
+            }
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                res.append((char) (s.charAt(i) + 1));
+            }
         }
-
-        //System.out.println(res.toString());
-
-
 
         return res.toString();
     }
@@ -52,6 +97,47 @@ public class Vigenere {
     static String decode(String s, String password) {
         return "";
     }
+
+    // Escapamos los caracteres especiales con acentos
+    static char escapeChar(char c){
+        char res = 0;
+
+        switch (c){
+            case 'Á': case 'À':
+                res = 'A';
+                break;
+            case 'È': case 'É':
+                res = 'E';
+                break;
+            case 'Ú' : case 'Ù':
+                res = 'U';
+                break;
+            case 'Í': case 'Ì':
+                res = 'I';
+                break;
+            case 'Ó': case 'Ò':
+                res = 'O';
+                break;
+            default:
+                res = c;
+                break;
+        }
+
+        return res;
+    }
+
+    // Asignamos un valor númerico a cada letra del alfabeto
+    static Map<Character, Integer> mapChars (String s) {
+        HashMap<Character, Integer> alfabeto = new HashMap<Character, Integer>();
+        s = s.toUpperCase();
+
+        for (int i = 0; i < s.length(); i++) {
+            alfabeto.put(escapeChar(s.charAt(i)), i);
+        }
+
+        return alfabeto;
+    }
+
 }
 
 
