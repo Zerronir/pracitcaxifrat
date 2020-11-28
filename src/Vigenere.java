660008ce91fc0f509244ab11499547c80651ff34
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-11-28 19:55:26 +0100

    Vigènere encode hecho

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -5,22 +5,32 @@ public class Vigenere {
     final String alfabeto = "abcdefghijklmnopqrstuvwxyz";
 
     public static void main(String[] args) {
-        String s = "YYYYY";
+        String s = "YYY";
         String pass = "AAA";
-        int a = 65;
+        int dif = 0;
+        int A = 65;
+        StringBuilder res = new StringBuilder();
+        if(s.length() > pass.length()){
+            StringBuilder ss = new StringBuilder(match(s, pass));
 
-        char c = 'Z';
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = 0; j < ss.length(); j++) {
 
-        int x = (c - a) + 1;
-        char z = (char) (a + x);
+                    dif = s.charAt(i) - ss.charAt(i) + 1;
+                    System.out.println("dif -> " + dif + " res: " + (char) (s.charAt(i) + dif));
 
-        if(z > 90){
-            System.out.println((char) (z - 26));
+                }
+            }
+
+        } else {
+            for (int i = 0; i < s.length(); i++) {
+                dif = s.charAt(i) - pass.charAt(i) + 1 - A;
+                char c = (char) (s.charAt(i) + dif);
+                res.append(c);
+            }
+            System.out.println(res.toString());
         }
 
-        System.out.println(z);
-
-        //System.out.println(pruebas(s, pass));
 
     }
 
@@ -76,19 +86,35 @@ public class Vigenere {
     }
 
     static String encode(String s, String password) {
-        s = s.toUpperCase();
         password = password.toUpperCase();
+        s = s.toUpperCase();
         StringBuilder res = new StringBuilder();
+        StringBuilder key = new StringBuilder(newPass(password,s));
 
-        if(s.length() > password.length()){
-            String newPass = match(s, password);
-            for (int i = 0; i < s.length(); i++) {
-                res.append((char) (s.charAt(i) +1 ));
+        // Libramos la frase de acentos y simbolos raros
+        StringBuilder clean = new StringBuilder(clean(s));
+
+        // Adaptamos la password para los espacios de las frases
+        StringBuilder finalPass = new StringBuilder(clean(key.toString()));
+
+        for (int i = 0; i < clean.length(); i++) {
+            // Calculamos el desplazamiento de la frase
+            int delta = finalPass.charAt(i) - 64;
+            // Creamos un caracter para saber en cual estamos y limpiar código
+            char c = clean.charAt(i);
+
+            if (c >= 65 && c <= 90){
+                c = (char) (delta + c);
+
+                if(c > 90) {
+                    c = (char) (c - 26);
+                }
+
+                res.append(c);
+            } else {
+                res.append(c);
             }
-        } else {
-            for (int i = 0; i < s.length(); i++) {
-                res.append((char) (s.charAt(i) + 1));
-            }
+
         }
 
         return res.toString();
@@ -126,16 +152,59 @@ public class Vigenere {
         return res;
     }
 
-    // Asignamos un valor númerico a cada letra del alfabeto
-    static Map<Character, Integer> mapChars (String s) {
-        HashMap<Character, Integer> alfabeto = new HashMap<Character, Integer>();
-        s = s.toUpperCase();
-
+    // Limpiamos la frase de caracteres extraños
+    static String clean(String s){
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            alfabeto.put(escapeChar(s.charAt(i)), i);
+
+            if(s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+                res.append(s.charAt(i));
+            } else {
+                res.append(escapeChar(s.charAt(i)));
+            }
+
         }
 
-        return alfabeto;
+        return res.toString();
+    }
+
+
+    // Adaptamos la password que tenemos a la palabra que queremos cifrar
+    static String newPass(String pass, String s){
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0, j = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+
+            if (c >= 65 && c <= 90 || c >= 192 && c <= 220) {
+
+                // Si la longitud de j llega a igualar a la de la contraseña original reiniciamos el contador
+                if (j == pass.length()) j=0;
+
+                // Buscamos si el caracter que está en i nos puede dar algún problema y lo escapamos
+                if(pass.charAt(j) == 181 || pass.charAt(j) == pass.charAt(j)
+                   || pass.charAt(j) == 144 || pass.charAt(j) == 212
+                   || pass.charAt(j) == 224 || pass.charAt(j) == 214
+                   || pass.charAt(j) == 222 || pass.charAt(j) == 227
+                   || pass.charAt(j) == 233 || pass.charAt(j) == 235) {
+                    res.append(escapeChar(pass.charAt(j)));
+
+                } else {
+                    // Si no es un caracter especial también lo añadimos a la nueva password
+                    res.append(pass.charAt(j));
+                }
+
+                // Incrementamos el contador de j
+                j++;
+
+            } else {
+                res.append(' ');
+            }
+        }
+
+        return res.toString();
     }
 
 }
