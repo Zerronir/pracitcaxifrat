5faebba25117d552acfdb00d136b9b3e7537a1e4
Raúl García-Verdugo Cardador <pqe0023@gmail.com>
2020-12-03 12:22:28 +0100

    Decypher try

--- src/Transposition.java
+++ src/Transposition.java
@@ -69,7 +69,59 @@ public class Transposition {
     }
 
     static String decypher(String s, int dim) {
-        return "";
+        // Creamos una string vacía donde guardaremos el resultado
+        StringBuilder res = new StringBuilder();
+
+        // Creamos contadores para los espacios
+        int contA = 0, contB = 0;
+
+        while((s.length() + contA) % dim != 0) contA++;
+
+        // Redondeamos el resultado por si tiene decimales
+        double decim = (double) s.length() / dim;
+        int filas = s.length() / dim;
+
+        if(decim > filas) filas++;
+
+        // Creamos una matriz con los tamaños de filas y columnas que hemos obtenido
+        char[][] matrix = new char[filas][dim];
+
+        // Sustituimos los valores null por asteríscos
+        if (contA > 0) {
+
+            for (int i = matrix.length; i > 0; i--) {
+                int innerCont = matrix[0].length - 1;
+
+                for (int j = contA; j > innerCont ; j--) {
+                    matrix[i - 1][innerCont] = '*';
+                    innerCont--;
+                }
+                break;
+            }
+        }
+
+
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+
+                if(matrix[j][i] == '*') continue;
+                matrix[j][i] = s.charAt(contB);
+                contB++;
+
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                if(matrix[i][j] == '*') continue;
+                res.append(matrix[i][j]);
+
+            }
+        }
+
+
+        return res.toString();
     }
 
     static String decypher(String s, String key) {
@@ -103,19 +155,27 @@ public class Transposition {
     }
 
 
-    private static char escapeChar(char c) {
-        switch (c) {
-            case 'À': case 'Á': case 'Ä':
-                return 'A';
-            case 'È': case 'É': case 'Ë':
-                return 'E';
-            case 'Ì': case 'Í': case 'Ï':
-                return 'I';
-            case 'Ò': case 'Ó': case 'Ö':
-                return 'O';
-            case 'Ù': case 'Ú': case 'Ü':
-                return 'U';
-            default: return c;
+    private int[] orderArray(int[] array) {
+
+        for (int i = array.length, x = 0; i > x; i--, x++) {
+            for (int j = 1; j < i; j++) {
+                if(array[j - 1] > array[j]){
+                    int canvi1 = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = canvi1;
+                }
+            }
+
+            for (int k = (i-1); k > x; k--) {
+                if(array[k - 1] > array[k]) {
+                    int canvi2 = array[k];
+                    array[k] = array[k - 1];
+                    array[k - 1] = canvi2;
+                }
+            }
         }
+
+        return array;
     }
+
 }
